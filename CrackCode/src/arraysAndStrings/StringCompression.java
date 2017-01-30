cba8bd0c312797509b530997803024727a461780
Kumar <kumarjivi@yahoo.com>
2017-01-29 19:54:00 -0800

    Updated Code

--- CrackCode/src/arraysAndStrings/StringCompression.java
+++ CrackCode/src/arraysAndStrings/StringCompression.java
@@ -1,8 +1,36 @@
 package arraysAndStrings;
 /**
  * @author Kumar
- * 
+ * Implement a method to perform basic String Compression using the count of the repeated
+ * Characters. If the "Compressed" string would not become smaller than the original String,
+ * method should return the original String.
  */
-public class CompressString {
-	
+public class StringCompression {
+	public String getCompressedString(String input) {
+		StringBuilder compressed = new StringBuilder();
+		if(input != null && input.trim().length() > 0) {
+			int currentCount = 1;
+			int index = 1;
+			int len = input.length();
+			char prev = input.charAt(0);
+			compressed.append(prev);
+			char curr = prev;
+			while(index < len) {
+				curr = input.charAt(index);
+				if(prev == curr) {
+					currentCount++;
+				} else {
+					compressed.append(currentCount);
+					prev = curr;
+					compressed.append(prev);
+					currentCount = 1;
+				}
+				index++;
+			}
+			if(compressed.length() >= len) {
+				return input;
+			}
+		}
+		return compressed.toString();
+	}
 }
